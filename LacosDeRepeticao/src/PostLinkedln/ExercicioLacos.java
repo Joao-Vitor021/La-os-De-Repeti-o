5256425d17b3fc0c989f4b8eb1b98256f2416b1e
joaov <joaov@DESKTOP-C6GB8J9.mshome.net>
2026-01-12 14:58:26 -0300

    Terceiro commit

---
+++ LacosDeRepeticao/src/PostLinkedln/ExercicioLacos.java
@@ -0,0 +1,24 @@
+package PostLinkedln;
+
+public class ExercicioLacos {
+	public static void main(String[] args) {
+
+		System.out.println("USO DO FOR");
+		// Contador de 0 a 10
+		for (int i = 0; i < 11; i++) {
+			System.out.print(i+" ");
+
+		}
+
+		// Contador de números pares
+		int n = 0;
+		System.out.println("\nUSO DO WHILE");
+		while (n < 20) {
+			n += 2;
+			System.out.print(n+" ");
+
+		}
+
+	}
+
+}
